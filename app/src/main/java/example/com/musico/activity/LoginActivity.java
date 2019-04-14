959263d329fbc5a1cdc3e30e33643dce15928a63
Mahima <mahima.6.ms@gmail.com>
2019-04-14 23:34:30 +0530

    add google sign in logic to login screen

--- app/src/main/java/example/com/musico/activity/LoginActivity.java
+++ app/src/main/java/example/com/musico/activity/LoginActivity.java
@@ -1,15 +1,92 @@
 package example.com.musico.activity;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import example.com.musico.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int RC_SIGN_IN = 1;
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    @BindView(R.id.signInButton)
+    SignInButton signInButton;
+    private GoogleSignInClient googleSignInClient;
+    private GoogleSignInAccount account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder()
+                .requestEmail()
+                .requestIdToken(getString(R.string.server_client_id))
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.signInButton:
+                signIn();
+                break;
+        }
+    }
+
+    private void signIn() {
+        Intent intent = googleSignInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleTaskResult(task);
+        }
+    }
+
+    private void handleTaskResult(Task<GoogleSignInAccount> task) {
+
+        try {
+            account = task.getResult(ApiException.class);
+            updateUI(account);
+        } catch (ApiException e) {
+            Log.e(TAG, "Login failed. Status code : " + e.getStatusCode());
+        }
+
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if (account != null) {
+            Log.i(TAG, "Login successful. User's email : " + account.getEmail() +
+                    " Token ID : " + account.getIdToken());
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
